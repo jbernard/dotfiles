e377456dc12a05d685b31cab0b6d62f2c0864f5a
Jon Bernard <jbernard@tuxion.com>
2014-07-07 14:08:33 -0400

    Use entry point for bin stub
    The script in bin/ remains for running directly from the source
    directory.

--- setup.py
+++ setup.py
@@ -37,5 +37,8 @@ setup(name='dotfiles',
           'Programming Language :: Python :: 3.1',
           'Programming Language :: Python :: 3.2',
       ),
-      scripts=['bin/dotfiles'],
+    entry_points = '''
+        [console_scripts]
+        dotfiles=dotfiles.cli:main
+    ''',
 )
