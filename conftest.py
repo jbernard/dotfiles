2d3afb27519c90949ffaaf24f7e0a7f4d4a50e93
Jon Bernard <jbernard@tuxion.com>
2016-01-04 05:45:39 -0500

    Nest repository in homedir for testing
    There is a bug when the home directory is not a parent of the
    repository, this will fixed shortly.

--- conftest.py
+++ conftest.py
@@ -2,16 +2,16 @@ import pytest
 from click.testing import CliRunner
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='function', params=[''])
 def home(request, tmpdir):
-    return tmpdir.ensure('home', dir=1)
+    return tmpdir.join(request.param)
 
 
 @pytest.fixture(scope='function')
-def repo(request, tmpdir):
+def repo(tmpdir):
     return tmpdir.ensure('repo', dir=1)
 
 
 @pytest.fixture(scope='function')
-def runner(request):
+def runner():
     return CliRunner()
