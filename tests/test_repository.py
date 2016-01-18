095e9a11d21e01d5175f2911f5e31e30f9ea14c6
Jon Bernard <jbernard@tuxion.com>
2016-01-18 09:49:07 -0500

    Make flake8 happy again

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -12,6 +12,7 @@ def test_init(repo, home):
     assert r.homedir == home
     assert repo.check(exists=1, dir=1)
 
+
 def test_str(repo, home):
     repo.ensure('.a')
     repo.ensure('.b')
@@ -23,24 +24,29 @@ def test_str(repo, home):
                                       home.join('.b'),
                                       home.join('.c')))
 
+
 def test_repr(repo):
     actual = '%r' % Repository(repo, None)
     expected = '<Repository local(\'%s\')>' % repo
     assert actual == expected
+
 
 def test_target_to_name(repo, home):
     actual = Repository(repo, home)._target_to_name(repo.join('.foo'))
     expected = home.join('.foo')
     assert actual == expected
 
+
 def test_name_to_target(repo, home):
     actual = Repository(repo, home)._name_to_target(home.join('.foo'))
     expected = repo.join('.foo')
     assert actual == expected
 
+
 @pytest.mark.xfail(reason='TODO')
 def test_dotfile():
     assert False
+
 
 def test_contents(repo, home):
 
@@ -54,6 +60,7 @@ def test_contents(repo, home):
     assert contents[0].target == target_a
     assert contents[1].target == target_b
     assert contents[2].target == target_c
+
 
 def test_nested_name_to_target(repo, home):
     r = Repository(repo, home)
