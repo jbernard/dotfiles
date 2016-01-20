9c48b33fe36b066e79443d9ebf29f1cf79ca1f21
Jon Bernard <jbernard@tuxion.com>
2016-01-20 11:21:11 -0500

    Small cleanup in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,12 @@ from click.testing import CliRunner
 
 @pytest.fixture(scope='function', params=['', 'home'])
 def home(request, tmpdir):
-    return tmpdir.ensure(request.param, dir=1)
+    return tmpdir.ensure_dir(request.param)
 
 
 @pytest.fixture(scope='function')
 def repo(tmpdir):
-    return tmpdir.ensure('repo', dir=1)
+    return tmpdir.ensure_dir('repo')
 
 
 @pytest.fixture(scope='function')
