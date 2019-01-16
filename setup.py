174f42ea32529d25a7790ae956e891c6ac15a99a
Jon Bernard <jbernard@jbernard.io>
2019-01-16 15:54:37 -0500

    Use flake8 instead of pep8 and pyflakes

--- setup.py
+++ setup.py
@@ -1,45 +1,40 @@
-from io import open
-from os import path
 from setuptools import setup, find_packages
 from dotfiles import __version__
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf8') as f:
-    long_description = f.read()
-
-test_requirements = [
-    'pytest',
-    'pytest-pep8',
-    'pytest-flakes',
-]
 
 setup(
     name='dotfiles',
     version=__version__,
     description='Easily manage your dotfiles',
-    long_description=long_description,
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/jbernard/dotfiles',
     author='Jon Bernard',
     author_email='jbernard@jbernard.io',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: ISC License (ISCL)'
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-    ],
+    license='ISC',
+    url='https://github.com/jbernard/dotfiles',
     packages=find_packages(),
-    install_requires=['click'],
+    tests_require=[
+        'pytest',
+        'pytest-flake8',
+    ],
     setup_requires=['pytest-runner'],
-    tests_require=test_requirements,
     entry_points={
         'console_scripts': [
             'dotfiles=dotfiles.cli:cli',
         ],
     },
+    install_requires=['click'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
     project_urls={
         'Bug Reports': 'https://github.com/jbernard/dotfiles/issues',
         'Source': 'https://github.com/jbernard/dotfiles',
